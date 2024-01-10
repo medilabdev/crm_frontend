3803037462d53e934eabbdad02d75ab05b159f24
jokoprasetio <joko12prasetio@gmail.com>
2024-01-10 09:08:14 +0700

    on progress chart

--- src/view/Dashboard/index.jsx
+++ src/view/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/Template/Sidebar";
 import Topbar from "../../components/Template/Topbar";
 import Main from "../../components/Template/Main";
@@ -10,8 +10,34 @@ import { Card } from "react-bootstrap";
 import { ChartVertikal } from "./ChartVertikal";
 import { ChartLineDoang } from "./ChartLineDoang";
 import { ChartWinLose } from "./ChartWinLose";
+import axios from "axios";
 
 function Dashboard() {
+  const token = localStorage.getItem('token');
+  const [stagingDeals, setStagingDeals] = useState([]);
+  const MAX_RETRIES = 3;
+  const getResultStage =  (retryCount = 0) => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/report/staging/deals`, {
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((res) => setStagingDeals(res.data.staging_data))
+    .catch(async(err) => {
+      if (err.response.data.message === "Unauthenticated") {
+        localStorage.clear();
+        window.location.href = "/login";
+      }
+      if(err.response.status === 429 && retryCount < MAX_RETRIES){
+        const delay = Math.pow(2, retryCount) * 1000;
+        await new Promise((resolve) => setTimeout(resolve, delay))
+        await getResultStage(retryCount + 1)
+      }
+    });
+  }
+  useEffect(() => {
+    getResultStage()
+  }, [token])
   return (
     <body id="body">
       <Topbar />
@@ -32,16 +58,14 @@ function Dashboard() {
           </div>
           <div className="row">
             <div>
-              <Card className="shadow">
-                <LineChart />
+            <Card className="shadow">
+                <ChartWinLose />
               </Card>
             </div>
           </div>
-          <div className="row">
+          {/* <div className="row">
             <div className="col-md-6">
-              <Card className="shadow">
-                <ChartBar />
-              </Card>
+             
             </div>
             <div className="col-md-6">
               <Card>
@@ -63,7 +87,7 @@ function Dashboard() {
                 <ChartWinLose />
               </Card>
             </div>
-          </div>
+          </div> */}
         </div>
         <Footer />
       </Main>
